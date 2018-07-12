65c942a0d2cacc213382047f456b0f2e37b70c02
Karyn Boyd <karynboyd74@gmail.com>
2018-07-12 15:19:32 -0500

    Adding Functions + some changes to Joins

--- Functions.java
+++ Functions.java
@@ -1,11 +1,30 @@
-/* 3.1 System Defined Functions
-Task – Create a function that returns the current time. Task – create a function that returns the length of a mediatype from the mediatype table
+/* SQL FUNCTIONS */
 
-3.2 System Defined Aggregate Functions
-Task – Create a function that returns the average total of all invoices Task – Create a function that returns the most expensive track
+/* ************************* 3.1 SYSTEM DEFINED FUNCTIONS *********************** */
+/* Task – Create a function that returns the current time. */
+ALTER SESSION SET TIME_ZONE = '-1:00';
 
-3.3 User Defined Scalar Functions
-Task – Create a function that returns the average price of invoiceline items in the invoiceline table
+SELECT CURRENT_TIMESTAMP
+FROM dual;
 
-3.4 User Defined Table Valued Functions
-Task – Create a function that returns all employees who are born after 1968. */
+
+/* Task – create a function that returns the length of a mediatype from the mediatype table */
+SELECT NAME, LENGTH2(NAME)
+FROM MEDIATYPE;
+
+
+/* ************************* 3.2 System Defined Aggregate Functions *********************** */
+/* Task – Create a function that returns the average total of all invoices */
+SELECT AVG(TOTAL)
+FROM INVOICE;
+
+/* Task – Create a function that returns the most expensive track */
+SELECT MAX(UNITPRICE)
+FROM INVOICELINE;
+
+/* ************************* 3.3 User Defined Scalar Functions *********************** */
+/* Task – Create a function that returns the average price of invoiceline items in the invoiceline table */
+
+
+/* ************************* 3.4 User Defined Table Valued Functions *********************** */
+/* Task – Create a function that returns all employees who are born after 1968. */
--- Joins.sql
+++ Joins.sql
@@ -1,10 +1,12 @@
-/* 7.1 INNER */
+/* SQL JOINS */
+
+/* ************************* 7.1 INNER *********************** */
 /* Inner Join CUSTOMERS & INVOICE - cols = custname & invoiceid */
 SELECT CUSTOMER.FIRSTNAME || CUSTOMER.LASTNAME AS Name, INVOICE.INVOICEID
 FROM CUSTOMER
 INNER JOIN INVOICE ON CUSTOMER.CUSTOMERID = INVOICE.INVOICEID;
 
-/* 7.2 OUTER */ 
+/* ************************* 7.2 OUTER *********************** */
 /* Outer Join on CUSTOMERS and INVOICE - cols customerid, firstname, lastname, invoiceid, total */
 SELECT  CUSTOMER.CUSTOMERID as ID,
         CUSTOMER.FIRSTNAME || CUSTOMER.LASTNAME as Name,
@@ -13,21 +15,21 @@ SELECT  CUSTOMER.CUSTOMERID as ID,
 FROM CUSTOMER
 LEFT OUTER JOIN INVOICE on CUSTOMER.CUSTOMERID = INVOICE.CUSTOMERID;
 
-/* 7.3 RIGHT */
+/* ************************* 7.3 RIGHT *********************** */
 /* Right join ALBUM and ARTIST - cols artistname & title */
 SELECT ALBUM.TITLE, ARTIST.NAME
 FROM ARTIST
 RIGHT OUTER JOIN ALBUM
 ON ALBUM.ARTISTID = ARTIST.ARTISTID;
 
-/* 7.4 CROSS */
+/* ************************* 7.4. CROSS *********************** */
 /* Cross Join ALBUM and ARTIST - sort by artist name ASC */
 SELECT *
 FROM ALBUM
     CROSS JOIN ARTIST
 ORDER BY ARTIST.NAME;
 
-/* 7.5 SELF JOIN */
+/* ************************* 7.5 SELF-JOIN *********************** */
 /* SELF JOIN self-join on the employee table, joining on the reportsto column. */
 SELECT FIRST.EMPLOYEEID, FIRST.LASTNAME || FIRST.FIRSTNAME AS Name, SECOND.REPORTSTO
 FROM EMPLOYEE FIRST, EMPLOYEE SECOND
